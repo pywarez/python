fcc27ac6dae029d92bf47c70e37514b1a29edf4f
pywarez <30953495+pywarez@users.noreply.github.com>
2017-08-22 08:09:26 +0200

    Update qnap.py

--- qnap.py
+++ qnap.py
@@ -4,7 +4,7 @@ import sys
 import time
 
 if len(sys.argv) < 2:
-    print "\nUsage: " + sys.argv[0] + " <HOST>\n"
+    print "\nUsage: " + sys.argv[0] + " <HOST>"
     print "USAGE: qnap.py host check/exploit"
     sys.exit()
 
